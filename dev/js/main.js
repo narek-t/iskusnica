82ee902df5caa225f04bd13872a29f3625c314f8
Narek-T <narek.tarverdyan@bk.ru>
2017-06-02 20:51:44 +0300

    popups done

--- dev/js/main.js
+++ dev/js/main.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
 		arrows: false,
 		dots: true,
 	});
+	$('.open-popup').magnificPopup({
+		type: 'inline',
+		fixedContentPos: true,
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+	});
 });
