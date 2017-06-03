ab03016ecb1ae41c6a287467c03346c7845de455
Narek-T <narek.tarverdyan@bk.ru>
2017-06-03 18:30:39 +0300

    front done

--- dev/js/main.js
+++ dev/js/main.js
@@ -14,4 +14,23 @@ $(document).ready(function() {
 		removalDelay: 300,
 		mainClass: 'my-mfp-slide-bottom'
 	});
+	$('.filter__checkbox input').on('change', function(event) {
+		if(this.checked) {
+			$(this).parents('.filter__checkbox').addClass('active');
+		}else {
+			$(this).parents('.filter__checkbox').removeClass('active');
+		}
+		var subFilter = $(this).parents('.filter__checkbox').next('.sub-filter');
+		if(subFilter.length && this.checked) {
+			subFilter.find('input').prop('checked', true);
+			subFilter.find('.filter__checkbox').addClass('active')
+		}else if(subFilter.length && !this.checked) {
+			subFilter.find('input').prop('checked', false);
+			subFilter.find('.filter__checkbox').removeClass('active')
+		}
+	});
+	$('.filter__title-wrapper').click(function(event) {
+		$(this).toggleClass('hidden');
+		$(this).next('.filter__checkboxes').toggleClass('hidden');
+	});
 });
